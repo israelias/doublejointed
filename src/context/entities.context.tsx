56b0d09c4ec04dff1c453b3ef92c70713e3c438d
israelias <israelias.js@gmail.com>
2021-11-02 17:41:42 +0800

    Refactor entities context for type safety

---
+++ src/context/entities.context.tsx
@@ -0,0 +1,106 @@
+import React, {
+  createContext,
+  useContext,
+  useCallback,
+  useMemo,
+  ProviderProps,
+} from "react"
+import { StaticQuery, graphql } from "gatsby"
+
+export type EntitiesContextType = {
+  entities?: Entity[]
+  getEntity: (id: any) => Entity | undefined
+  getUrl: (id: any) => string | null
+  getName: (id: any) => any
+}
+
+export const EntitiesContext = createContext<EntitiesContextType>(undefined!)
+
+const entitiesQuery = graphql`
+  query {
+    entities: surveyApi {
+      entities {
+        id
+        name
+        homepage
+        mdn {
+          url
+        }
+      }
+    }
+  }
+`
+
+const findEntity = (entities: Entity[], key: string) =>
+  entities.find(({ id, name }) => {
+    const lowerCaseKey = key.toLowerCase()
+    const idMatch = id.toLowerCase() === lowerCaseKey
+    const nameMatch = name.toLowerCase() === lowerCaseKey
+
+    return idMatch || nameMatch
+  })
+
+const EntitiesContextProviderInner = (
+  props: ProviderProps<EntitiesContextType>
+) => {
+  const {
+    value: { entities },
+  } = props
+  const getEntity = useCallback(
+    id => {
+      const entity = findEntity(entities!, id)
+
+      return entity
+    },
+    [entities]
+  )
+
+  const getName = useCallback(
+    id => {
+      const entity = findEntity(entities!, id)
+
+      return (entity && entity.name) || id
+    },
+    [entities]
+  )
+
+  const getUrl = useCallback(
+    id => {
+      const entity = findEntity(entities!, id)
+
+      return (entity && entity.homepage) || null
+    },
+    [entities]
+  )
+
+  const value = useMemo(
+    () => ({
+      getEntity,
+      getName,
+      getUrl,
+    }),
+    [getName, getUrl, getEntity]
+  )
+
+  return (
+    <EntitiesContext.Provider value={value}>
+      {props.children}
+    </EntitiesContext.Provider>
+  )
+}
+
+export const EntitiesContextProvider = (
+  props: ProviderProps<EntitiesContextType>
+) => {
+  return (
+    <StaticQuery query={entitiesQuery}>
+      {({ entities: { entities } }) => (
+        <EntitiesContextProviderInner value={entities}>
+          {props.children}
+        </EntitiesContextProviderInner>
+      )}
+    </StaticQuery>
+  )
+}
+
+export const useEntities = () => useContext(EntitiesContext)
